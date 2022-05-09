4f4853290c6128009364fdabdf4bb1366dfd3579
Graciela Parente <graciparentte@gmail.com>
2022-05-08 22:45:57 -0300

    feat: create GET endpoint to bring all Pessoas from database

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,2 +1,8 @@
-const { Route } = require('express');
+const { Router } = require('express');
 const PessoaController = require('../controllers/PessoaController');
+
+const router = Router();
+
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+
+module.exports = router;
