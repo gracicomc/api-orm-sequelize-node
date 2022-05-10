b5a7c3b26af58e1bf642a447f55fb958120aab8b
Graciela Parente <graciparentte@gmail.com>
2022-05-10 11:29:24 -0300

    feat: create POST pessoa method

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,6 +4,7 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
-router.get('/pessoa/:id', PessoaController.pegaUmaPessoa);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+router.post('/pessoas', PessoaController.criaPessoa);
 
 module.exports = router;
