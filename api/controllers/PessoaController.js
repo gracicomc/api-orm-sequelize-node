709728f2284b11d2aae98b5c7e4d0d65dc39c778
Graciela Parente <graciparentte@gmail.com>
2022-05-10 12:40:22 -0300

    feat: create PATCH method

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -31,6 +31,26 @@ class PessoaController {
             return res.status(500).json(err.message);
         }
     }
+
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params;
+        const novasInfos = req.body;
+        try {
+            await database.Pessoas.update(novasInfos, {
+                where: { id: Number(id) },
+            });
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: { id: Number(id) },
+            });
+            return res.status(200).json(pessoaAtualizada);
+        } catch (err) {
+            return res.status(500).json(err.message);
+        }
+    }
+
+    static async excluiPessoa(req, res) {
+        const { id } = req.params;
+    }
 }
 
 module.exports = PessoaController;
